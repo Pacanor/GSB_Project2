e4360665b11b3cd702187110baac5f1523e51816
MalamaKangwa <Malama.Kangwa@szi.gov.zm>
2020-01-13 18:05:21 +0200

    Feature: Added Square Root and Tests

---
+++ BasicOps/SquareRoot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicOps
+{
+    public class SquareRoot
+    {
+        public static int Root(int a)
+        {
+            double b = Math.Sqrt(a);
+            int c = Convert.ToInt32(b);
+            return c;
+        }
+
+        public static double Root(double a)
+        {
+            double b = Math.Sqrt(a);
+            return b;
+        }
+
+
+        public static double[] Root(double[] arrayA)
+        {
+
+            int l = Helpers.Array.arrayLength(arrayA);
+            double[] c = new double[l];
+            int i = 0;
+
+            foreach (double a in arrayA)
+            {
+                c[i] = Root(a);
+                i++;
+            }
+            return c;
+        }
+    }
+}
---
+++ BasicOpsTests/SquareRootTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicOps;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicOps.Tests
+{
+    [TestClass()]
+    public class SquareRootTests
+    {
+        private readonly int h = 144;
+        private readonly double i = 2.25;
+        private readonly double[] arrayC = { 1.44, 6.25, 38.44 };
+
+        [TestMethod()]
+        public void SqrRootIntTest()
+        {
+            int c = SquareRoot.Root(h);
+            Assert.AreEqual(12, c);
+        }
+
+        public void SqrRootDoubleTest()
+        {
+            double c = SquareRoot.Root(i);
+            Assert.AreEqual(1.5, c);
+        }
+
+
+        [TestMethod()]
+        public void SqrRootDoubleArrayTest()
+        {
+            double[] x = { 1.2, 2.5, 6.2 };
+            double[] c = SquareRoot.Root(arrayC);
+            for (int i = 0; i < c.Length; i++)
+            {
+                Assert.AreEqual(x[i], c[i]);
+            }
+
+        }
+    }
+}
