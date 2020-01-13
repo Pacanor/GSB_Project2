4bd329fafdfaeb042f7d85af23f87a7d18c4ed0a
pacanor <papiasb@gmail.com>
2020-01-13 14:39:54 +0200

    Feature: Added static, object, interface and tests for Mean

---
+++ StatisticOps/StatMean.cs
@@ -0,0 +1,17 @@
+﻿
+namespace StatisticOps
+{
+    public class StatMean
+    {
+        public static dynamic Mean(dynamic values)
+        {
+            var sum = 0;
+            foreach(dynamic a in values)
+            {
+                sum += a;
+            }
+            var mean = sum/ values.Length;
+            return mean;
+        }
+    }
+}
